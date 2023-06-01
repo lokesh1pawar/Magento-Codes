d439a24ca4c82b6716a564519cb98d3b10f78438
Lokesh pawar <lokesh1pawar@gmail.com>
2023-06-01 18:59:38 +0530

    Update Data.php

--- Restrict_Product/RestrictProduct/Helper/Data.php
+++ Restrict_Product/RestrictProduct/Helper/Data.php
@@ -12,7 +12,6 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
 {
     public const CUSTOMER_GROUP_RESTRICT = 'restrict_product/general/restrict_customer_group';
 
-    // protected ScopeConfigInterface $scopeConfig;
     protected CustomerSession $customerSession;
     protected StoreManagerInterface $storeManager;
     protected $productRepository;
@@ -21,20 +20,20 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
     public function __construct(
         \Magento\Framework\App\Helper\Context $context,
         StoreManagerInterface $storeManager,
-        // ScopeConfigInterface $scopeConfig,
         CustomerSession $customerSession,
         \Magento\Catalog\Api\ProductRepositoryInterface $productRepository
 
     ) {
         parent::__construct($context);
         $this->customerSession = $customerSession;
-        // $this->scopeConfig = $scopeConfig;
         $this->storeManager = $storeManager;
         $this->productRepository = $productRepository;
 
     }
+    
+        // For Customer Group values from configuration 
 
-    public function isCustomerInAllowedGroup(): bool
+      public function isCustomerInAllowedGroup(): bool
     {
         if ($this->customerSession->isLoggedIn()) {
             $customerGroup = $this->customerSession->getCustomer()->getGroupId();
@@ -46,40 +45,61 @@ class Data extends \Magento\Framework\App\Helper\AbstractHelper
             );
 
             $groupArray = explode(",", (string)$groupList);
-            // print_r($groupArray);
-            // exit;
 
             if (in_array($customerGroup, $groupArray)) {
                 return true;
             }
         }
+             return false;
+     }
 
-        return false;
-    }
+            // For Comma Saprated SKU from configuration
 
-    public function isRestrictedSku($sku)
-    {
-        $restrictedSkus = $this->scopeConfig->getValue(
-            'restrict_product/general/restricted_skus',
-            \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
-            $this->storeManager->getStore()->getStoreId()
-        );
+            public function isRestrictedSku($sku)
+            {
+                $restrictedSkus = $this->scopeConfig->getValue(
+                    'restrict_product/general/restricted_skus',
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                    $this->storeManager->getStore()->getStoreId()
+                );
 
-        // $restrictedSkuArray = str_getcsv($restrictedSkus);
-        $restrictedSkuArray = explode(",", (string)$restrictedSkus);
-        // print_r($restrictedSkuArray);
-        // exit;
+                $restrictedSkuArray = explode(",", (string)$restrictedSkus);
 
-        if (in_array($sku, $restrictedSkuArray, true)) {
-            return true;
-        }
+                if (in_array($sku, $restrictedSkuArray, true)) {
+                    return true;
+                }
+                return false;
+            }
 
-        return false;
-    }
+            // For Custom URL path from configuration
 
-    public function getProductById($id)
-{
-    return $this->productRepository->getById($id);
-}
+        public function cmsRedirectUrl()
+            {
+                $cmsRedirect = $this->scopeConfig->getValue(
+                    'restrict_product/general/restricted_csm_redirect',
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                    $this->storeManager->getStore()->getStoreId()
+                );
+                return $cmsRedirect;
+            }
+
+            // For Custom Message from configuration
+
+        public function customMsg()
+            {
+                $msg = $this->scopeConfig->getValue(
+                    'restrict_product/general/restricted_after_msg',
+                    \Magento\Store\Model\ScopeInterface::SCOPE_STORE,
+                    $this->storeManager->getStore()->getStoreId()
+                );
+                return $msg;
+            }
+
+            // For get Current Product ID
+
+            public function getProductById($id)
+                {
+                    return $this->productRepository->getById($id);
+                }
 
 }
